64a27e93b6f0b963748f7eb7441b68f8430a6cd3
Mateo Montoya <matteomontoya777@gmail.com>
2026-08-06 15:14:45 -0500

    Configurar navegación principal

--- app/lista_tareas/src/app/app.routes.ts
+++ app/lista_tareas/src/app/app.routes.ts
@@ -2,12 +2,35 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: '',
+    redirectTo: 'tabs/inicio',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'tabs',
+    loadComponent: () =>
+      import('./tabs/tabs.page').then((m) => m.TabsPage),
+    children: [
+      {
+        path: 'inicio',
+        loadComponent: () =>
+          import('./inicio/inicio.page').then((m) => m.InicioPage),
+      },
+      {
+        path: 'tareas',
+        loadComponent: () =>
+          import('./tareas/tareas.page').then((m) => m.TareasPage),
+      },
+      {
+        path: 'categorias',
+        loadComponent: () =>
+          import('./categorias/categorias.page').then((m) => m.CategoriasPage),
+      },
+      {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
---
+++ app/lista_tareas/src/app/tabs/tabs.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet
+} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
+  standalone: true,
+  imports: [
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet
+  ]
+})
+export class TabsPage {}
