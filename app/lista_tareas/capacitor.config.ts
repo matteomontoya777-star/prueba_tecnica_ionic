c981f5850bc714625adf64b820c1723e6dcec46c
Mateo Montoya <matteomontoya777@gmail.com>
2026-08-06 13:36:15 -0500

    Preparar estructura inicial del proyecto

---
+++ app/lista_tareas/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lista_tareas',
+  webDir: 'www'
+};
+
+export default config;
